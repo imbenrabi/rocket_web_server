610dcbe87247cde8a101e916a4e790ad64bf282a
imbenrabi <imbenrabi@gmail.com>
2021-11-20 16:32:00 +0200

    all crates addition and latest nightly

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,29 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+use rusqlite::Connection;
+
 #[macro_use]
 extern crate rocket;
 
 #[get("/")]
 fn index() -> &'static str {
-    "Hello, world!"
+    "Hello from Rust!"
 }
 
-#[launch]
-fn rocket() -> _ {
-    rocket::build().mount("/", routes![index])
+fn main() {
+    {
+        let db_connection = Connection::open("data.sqlite").unwrap();
+
+        db_connection
+            .execute(
+                "create table if not exists todo_list (
+                    id integer primary key,
+                    item varchar(64) not null
+                );",
+                rusqlite::NO_PARAMS,
+            )
+            .unwrap();
+    }
+
+    rocket::ignite().mount("/", routes![index]).launch();
 }
